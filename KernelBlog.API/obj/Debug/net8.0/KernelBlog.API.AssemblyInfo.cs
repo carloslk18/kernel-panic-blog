5c954e77479b3d7ac049efffd7748a3a9c5a543b
Carlos Barbosa <carloslk18@gmail.com>
2025-07-03 02:05:07 -0300

    docs: update readme with new info and lang

--- KernelBlog.API/obj/Debug/net8.0/KernelBlog.API.AssemblyInfo.cs
+++ KernelBlog.API/obj/Debug/net8.0/KernelBlog.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KernelBlog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7188df76bb4e46e456e69366966e93f19e5597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57a50a9cd8bb7668cf06ccc89223ddc6c2348828")]
 [assembly: System.Reflection.AssemblyProductAttribute("KernelBlog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KernelBlog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
